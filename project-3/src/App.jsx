de80a21947cb980c58e0da8dd3e94cd8adcb07cf
Rahul Patidar <patidarr420@gmail.com>
2025-06-16 16:06:52 +0530

    first page complite

--- project-3/src/App.jsx
+++ project-3/src/App.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { useState } from "react";
+import StartGame from "./components/StartGame";
+import GamePlay from "./components/GamePlay";
+
 
 const App = () => {
+
+  const [isGameStarted, setIsGameStarted] = useState(false);
+  const toggleGamePlay = () => {
+    setIsGameStarted((prev) => !prev);
+  }
+
   return (
-    <div>App</div>
+    <div>
+      {isGameStarted ? <GamePlay /> : <StartGame toggle={toggleGamePlay}/>}
+    </div>
   )
 }
 
---
+++ project-3/src/components/GamePlay.jsx
@@ -0,0 +1,10 @@
+
+const GamePlay = () => {
+  return (
+    <div>
+        <img src="images/dices.png" alt="" />
+    </div>
+  )
+}
+
+export default GamePlay
